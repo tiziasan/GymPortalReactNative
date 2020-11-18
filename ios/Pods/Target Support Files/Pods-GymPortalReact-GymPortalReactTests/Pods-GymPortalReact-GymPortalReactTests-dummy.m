50aa3c8c642d9e02476106a96ce6c5b669795648
tiziasan <tizianosantilli@gmail.com>
2020-11-18 23:20:39 +0100

    added code

---
+++ ios/Pods/Target Support Files/Pods-GymPortalReact-GymPortalReactTests/Pods-GymPortalReact-GymPortalReactTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GymPortalReact_GymPortalReactTests : NSObject
+@end
+@implementation PodsDummy_Pods_GymPortalReact_GymPortalReactTests
+@end
